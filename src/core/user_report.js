97d83d20d1b7b36251693055fbc67190748b5cc7
Santosh Kumar S <szasanto@amazon.com>
2021-11-25 21:51:15 +0530

    filter

--- src/core/user_report.js
+++ src/core/user_report.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import { Helmet } from "react-helmet";
 import "../assets/css/admin_dashboard/user_report.css";
-import filter from "../assets/images/filtericon.svg";
+import filtericon from "../assets/images/filtericon.svg";
 import { VscHistory } from "react-icons/vsc";
+import { IoCloseOutline } from "react-icons/io5";
 
 export default function User_report() {
+  const [filter, setFilter] = useState(false);
+
   const [data, setData] = useState([
     {
       name: "Alfreds Futterkiste",
@@ -63,34 +66,38 @@ export default function User_report() {
       ldate: "12/06/2020",
     },
     {
-        name: "Alfreds Futterkiste",
-        acntnumber: 567362925,
-        NumofBooking: 17,
-        Balance: "$6,500.00",
-        ldate: "28/10/2012",
-      },
-      {
-        name: "Francisco Chang",
-        acntnumber: 567362926,
-        NumofBooking: 12,
-        Balance: "$266.00",
-        ldate: "12/06/2020",
-      },
-      {
-        name: "Alfreds Futterkiste",
-        acntnumber: 567362925,
-        NumofBooking: 17,
-        Balance: "$6,500.00",
-        ldate: "28/10/2012",
-      },
-      {
-        name: "Francisco Chang",
-        acntnumber: 567362926,
-        NumofBooking: 12,
-        Balance: "$266.00",
-        ldate: "12/06/2020",
-      },
+      name: "Alfreds Futterkiste",
+      acntnumber: 567362925,
+      NumofBooking: 17,
+      Balance: "$6,500.00",
+      ldate: "28/10/2012",
+    },
+    {
+      name: "Francisco Chang",
+      acntnumber: 567362926,
+      NumofBooking: 12,
+      Balance: "$266.00",
+      ldate: "12/06/2020",
+    },
+    {
+      name: "Alfreds Futterkiste",
+      acntnumber: 567362925,
+      NumofBooking: 17,
+      Balance: "$6,500.00",
+      ldate: "28/10/2012",
+    },
+    {
+      name: "Francisco Chang",
+      acntnumber: 567362926,
+      NumofBooking: 12,
+      Balance: "$266.00",
+      ldate: "12/06/2020",
+    },
   ]);
+
+  function handleFilter() {
+    setFilter(!filter);
+  }
 
   return (
     <>
@@ -100,25 +107,67 @@ export default function User_report() {
       <div className="User_report_container flex-grow-1">
         <div className="User_report_detail_entry">
           <div className="User_report_title_text">User Report</div>
-          <div className="User_report_search_container">
-            <label
-              for="User_report_uid"
-              className="User_report_search_input_label"
-            >
-              {" "}
-              Name/Account
-            </label>
-            <input
-              type="text"
-              name="userid"
-              id="User_report_uid"
-              className="User_report_search_input"
-            />
-            <div className="User_report_get_button"> Get </div>
-            <div className="User_report_filter_button">
-              {" "}
-              Filter <img src={filter} alt="Munidex_filter" />
+          <div className="User_report_filter_search_container">
+            <div className="User_report_search_container">
+              <label
+                for="User_report_uid"
+                className="User_report_search_input_label"
+              >
+                {" "}
+                Name/Account
+              </label>
+              <input
+                type="text"
+                name="userid"
+                id="User_report_uid"
+                className="User_report_search_input"
+              />
+              <div className="User_report_get_button"> Get </div>
+              <div className="User_report_filter_button" onClick={handleFilter}>
+                {" "}
+                Filter <img src={filtericon} alt="Munidex_filter" />
+              </div>
             </div>
+            {filter && (
+              <div className="User_report_filter_card p-3">
+                <div className="User_report_filter_card_segment1 mt-4 mb-4 row">
+                  <div className="User_report_filter_card_segment1_text_ps col-4"> Payment Status</div>
+                  <div className="col-3">
+                  <input
+                    type="checkbox"
+                    id="pending"
+                    name="pending"
+                    className="User_report_filter_card_segment1_input_pending"
+                  />
+                  <label for="pending" className="User_report_filter_card_segment1_text_pending"> Pending </label>
+                  </div>
+                  <div className="col-3">
+                  <input
+                    type="checkbox"
+                    id="completed"
+                    name="completed"
+                    className="User_report_filter_card_segment1_input_completed"
+                  />
+                  <label for="pending" className="User_report_filter_card_segment1_text_completed"> Completed </label>
+                  </div>
+                  <div className="col-2" onClick={handleFilter} style={{display:"flex", justifyContent:"flex-end", paddingRight:"5%", position:"relative", bottom:"100%"}} >
+                  <IoCloseOutline size={20}/>
+                  </div>
+                </div>
+                <div className="User_report_filter_card_segment2 mb-4 row">
+                  <div className="User_report_filter_card_segment2_text_date col-4">Date</div>
+                  <div className="User_report_filter_card_segment2_date_picker col-6"> Date picker here </div>
+                  <div className="col-2"></div>
+                </div> 
+                <div className="User_report_filter_card_segment3 mb-2">
+                  <div className="User_report_filter_card_segment3_text_caf col-3"> clear all filters </div>
+                  <div style={{display:"flex"}}>
+                  <div className="User_report_filter_card_segment3_text_cancel me-3"> Cancel </div>
+                  <div className="User_report_filter_card_segment3_text_apply me-3"> Apply </div>
+                  </div>
+                </div>   
+              </div>
+            )}
           </div>
         </div>
         <div className="User_report_text_recent">
@@ -142,7 +191,9 @@ export default function User_report() {
                   <td>{userdata.NumofBooking}</td>
                   <td>{userdata.Balance}</td>
                   <td>{userdata.ldate}</td>
-                  <td><span className="User_report_controls_view">View</span></td>
+                  <td>
+                    <span className="User_report_controls_view">View</span>
+                  </td>
                 </tr>
               );
             })}
