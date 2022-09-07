a2f9ea95cd563c25976bf656892f17cf2367d240
kaamil <kaamil312@gmail.com>
2022-09-07 18:58:06 +0530

    pay1

--- src/core/user_dashboard.js
+++ src/core/user_dashboard.js
@@ -518,9 +518,7 @@ export default function User_dashboard() {
         data: body,
         port: 443,
         headers: {
-          "Content-Type": "application/json",
-          Cookie:
-            "connect.sid=s%3Ajn1ZAMIq3w-AOZiwO4qGDqsbFvfd6OT7.4xwH5hCrPjKBetTh6rW8NogYksb84jMRdpfzNUJibN0",
+          "Content-Type": "application/json",       
         },
         json: true,
         withCredentials: true
